d8a510843ffc2b80bb9fcbaec5cdcb97076cafa6
Mickael <mickael.guy.pro@gmail.com>
2025-05-16 10:48:12 +0200

    Changing port

--- jwt-express-server.js
+++ jwt-express-server.js
@@ -23,7 +23,7 @@ const USER = {
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5174", // ou "*" en dev
+    origin: "http://localhost:5173", // ou "*" en dev
     methods: ["POST", "GET", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, // si tu envoies des cookies
